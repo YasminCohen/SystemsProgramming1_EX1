ee4fb51d6a81932ef0b2b7077de285b7514df5d7
2shlomi9 <118685589+2shlomi9@users.noreply.github.com>
2022-11-22 21:40:44 +0200

    fix main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
 {
     int a, b;
     scanf("%d   %d", &a, &b);
-    printf("Armstrong Numbers between %d , %d is :", a, b);
+    printf("The Armstrong numbers are:");
     for (int i = a; i <= b; i++)
     {
         if (isArmstrong(i) == 1)
@@ -15,7 +15,7 @@ int main()
         }
     }
     printf("\n");
-    printf("The Palindromes Numbers between %d , %d is :", a, b);
+    printf("The Palindromes are:");
     for (int i = a; i <= b; i++)
     {
         if (isPalindrome(i) == 1)
@@ -24,7 +24,7 @@ int main()
         }
     }
     printf("\n");
-    printf("The Primes Numbers between %d , %d is :", a, b);
+    printf("The Prime numbers are:");
     for (int i = a; i <= b; i++)
     {
         if (isPrime(i) == 1)
@@ -33,7 +33,7 @@ int main()
         }
     }
     printf("\n");
-    printf("The Strong Numbers between %d , %d is :", a, b);
+    printf("The Strong numbers are:");
     for (int i = a; i <= b; i++)
     {
         if (isStrong(i) == 1)
